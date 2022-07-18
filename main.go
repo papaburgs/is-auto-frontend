7503627678710206d3003a23c47b22a1967b3068
David Olson <david.olson@sasktel.com>
2022-07-18 16:40:48 -0600

    WIP: work on authentication with keys

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
 	"github.com/evertras/bubble-table/table"
+	"github.com/gliderlabs/ssh"
 	"github.com/papaburgs/is-auto-frontend/pkg/stnetbox"
 )
 
@@ -33,10 +43,15 @@ type server struct {
 }
 
 type model struct {
-	tableModel table.Model
+	tableModel    table.Model
+	term          string
+	width         int
+	height        int
+	authenticated bool
 }
 
 func (m model) Init() tea.Cmd {
+	log.Println("start init")
 	return nil
 }
 
@@ -103,6 +118,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "esc", "q":
 			cmds = append(cmds, tea.Quit)
 		}
+	default:
+		log.Printf("%s", msg)
 
 	}
 	return m, tea.Batch(cmds...)
@@ -110,6 +127,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	body := strings.Builder{}
+
+	if m.width < 120 {
+		return "Width needs to be greater than 120"
+	}
+
+	if m.authenticated {
+		body.WriteString("Valid auth\n")
+	} else {
+		body.WriteString("READ ONLY\n")
+	}
 
 	body.WriteString("Press space/enter to select a row, q or ctrl+c to quit\n")
 
@@ -129,10 +156,57 @@ func (m model) View() string {
 	return body.String()
 }
 
-func main() {
-	s, _ := stnetbox.GetInv()
+// authenticate will read the files in a directory
+//  - which can be populated in a config map
+// each file is a username, contents are the pub key
+// if the pub key matches the one that we authenticated with and the filename
+// matches the user that signed in, we are authenticated, if not, result if false
+func authenticate(s ssh.Session) bool {
+	// TODO working here
+	files, err := ioutil.ReadDir("./keys")
+	if err != nil {
+		log.Println("Could not read key directory")
+		log.Println(err)
+		return false
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if file.Name() == s.User() {
+			contents, err := os.ReadFile(file.Name())
+			if err != nil {
+				log.Println("Could not read %s - %s", file.Name(), err)
+				continue
+			}
+			thisPublicKey, err := ssh.ParsePublicKey(contents)
+			if err != nil {
+				log.Println("Could not parse key from %s - %s", file.Name(), err)
+				continue
+			}
+			if ssh.KeysEqual(s.PublicKey(), thisPublicKey) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+const (
+	host = "0.0.0.0"
+	port = 39722
+)
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+
+	pty, _, active := s.Pty()
+	if !active {
+		wish.Fatalln(s, "no active terminal, skipping")
+		return nil, nil
+	}
+	servers, _ := stnetbox.GetInv()
 	rows := []table.Row{}
-	for i, item := range s {
+	for i, item := range servers {
 		thisServer := table.NewRow(table.RowData{
 			columnKeyName:   item.Name,
 			columnKeyStatus: "unknown",
@@ -145,10 +219,43 @@ func main() {
 		}
 	}
 
-	p := tea.NewProgram(NewModel(rows), tea.WithAltScreen())
+	m := NewModel(rows)
+	m.term = pty.Term
+	m.width = pty.Window.Width
+	m.height = pty.Window.Height
+	m.authenticated = authenticate(s)
 
-	if err := p.Start(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithPublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool { return true }),
+		wish.WithMiddleware(
+			bm.Middleware(teaHandler),
+			lm.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Starting SSH server on %s:%d", host, port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	<-done
+	log.Println("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
 	}
 }
